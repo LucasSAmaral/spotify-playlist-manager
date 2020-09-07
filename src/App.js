475fcf75c485ae86728a87e829c79dcc38d9401c
Lucas <lucassantosamaral@hotmail.com>
2020-09-07 19:07:45 -0300

    Add useEffect to set access_token cookie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cookie from "js-cookie";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
@@ -8,14 +8,17 @@ import MyPlaylists from "./pages/MyPlaylists/MyPlaylists.component";
 import Header from "./components/Header/Header.component";
 
 function App() {
-  const hasAccessToken = Cookie.get("access_token");
+  const [accessToken, setAccessToken] = useState();
+  useEffect(() => {
+    setAccessToken(Cookie.get("access_token"));
+  }, []);
   return (
     <>
-      {hasAccessToken && <Header />}
+      {accessToken && <Header />}
       <Switch>
         <Route exact path="/" component={RedirectComponent} />
         <Route path="/login">
-          {hasAccessToken ? <Redirect to="/my-playlists" /> : <Login />}
+          {accessToken ? <Redirect to="/my-playlists" /> : <Login />}
         </Route>
         <Route path="/my-playlists" component={MyPlaylists} />
       </Switch>
