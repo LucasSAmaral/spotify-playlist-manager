ba11522d20194b42032b7e22016520af1d260c82
Lucas <lucassantosamaral@hotmail.com>
2020-09-29 00:02:44 -0300

    Add prop minWidth on TextLoading component

--- src/components/TextLoading/TextLoading.component.jsx
+++ src/components/TextLoading/TextLoading.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./TextLoading.scss";
 
-const TextLoading = ({ width, height }) => {
+const TextLoading = ({ width, height, minWidth }) => {
   return (
     <div
       className="text-loading"
-      style={{ "--width": width, "--height": height }}
+      style={{ "--width": width, "--height": height, "--min-width": minWidth }}
     />
   );
 };
