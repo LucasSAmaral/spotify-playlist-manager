38624996c27b7ac92330ff0c1f51faefdb500d5d
Lucas <lucassantosamaral@hotmail.com>
2020-09-29 00:03:36 -0300

    Add playlist description with text loading

--- src/pages/Playlist/Playlist.container.jsx
+++ src/pages/Playlist/Playlist.container.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { getPlaylistRequest } from "./Playlist.request";
 import { useQuery } from "react-query";
 import { PlaylistExtractor } from "./Playlist.extractor";
+import TextLoading from "../../components/TextLoading/TextLoading.component";
 
 const Playlist = () => {
   const { id } = useParams();
@@ -18,12 +19,23 @@ const Playlist = () => {
 
   return (
     <div className="playlist">
-      <h3>{PlaylistInfo?.name ?? "Loading"}</h3>
+      <h3>{PlaylistInfo?.name ?? "Loading..."}</h3>
 
       <div className="playlist-info">
         <figure>
           <img src={PlaylistInfo.image} alt="" />
         </figure>
+
+        <div className="playlist-description">
+          <h2>Description:</h2>
+          <h4>
+            {PlaylistInfo.description !== ""
+              ? PlaylistInfo?.description ?? (
+                  <TextLoading width="100%" height="18px" minWidth="395px" />
+                )
+              : "The playlist description wasn't written."}
+          </h4>
+        </div>
       </div>
     </div>
   );
