d90613ba69b29a83230b5b7470e6561005d6ce7b
Lucas <lucassantosamaral@hotmail.com>
2020-10-22 04:05:28 -0300

    Alter redirect_uri

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,3 +1,5 @@
 export const ClientId = "016d11c7be804f3d9e87892b4e12c821";
 export const ClientSecret = "942a12be73f74ca2b39e2c45c3dc7749";
-export const redirect_uri = "http://localhost:3000/";
+// export const redirect_uri = "http://localhost:3000/";
+export const redirect_uri =
+  "https://lucassamaral.github.io/spotify-playlist-manager/#/";
