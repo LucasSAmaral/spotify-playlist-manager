6c4e7b11edc76b5ada761a598535a3d5f3da7b51
Lucas <lucassantosamaral@hotmail.com>
2020-11-06 00:17:15 -0300

    Create searchAlbumsExtractor and searchEpisodesExtractor and refact searchTracksExtractor

--- src/components/Search/Search.extractor.js
+++ src/components/Search/Search.extractor.js
@@ -9,8 +9,8 @@ export const searchTracksExtractor = (searchData) => {
         name: curr.name,
         artist: curr.artists[0].name,
         album: { name: curr.album.name, image: curr.album.images[2].url },
-        trackId: curr.id,
-        preview_url: curr.preview_url,
+        uri: curr.uri,
+        preview: curr.preview_url,
       },
     ];
   }, []);
@@ -18,10 +18,30 @@ export const searchTracksExtractor = (searchData) => {
 
 export const searchAlbumsExtractor = (searchData) => {
   const safeSearchData = propOr([], "0", searchData);
-  console.log("albums extractor", safeSearchData);
+  return safeSearchData.albums?.items.reduce((acc, curr) => {
+    return [
+      ...acc,
+      {
+        name: curr.name,
+        artist_name: curr.artists[0].name,
+        image: curr.images[2].url,
+        uri: curr.uri,
+      },
+    ];
+  }, []);
 };
 
 export const searchEpisodesExtractor = (searchData) => {
   const safeSearchData = propOr([], "0", searchData);
-  console.log("episodes extractor", safeSearchData);
+  return safeSearchData.episodes?.items.reduce((acc, curr) => {
+    return [
+      ...acc,
+      {
+        name: curr.name,
+        preview: curr.audio_preview_url,
+        uri: curr.uri,
+        image: curr.images[2].url,
+      },
+    ];
+  }, []);
 };
